f8f9a873d1dfb74ed943a2c6928a36f1b8964529
mojtabavahidinasab <mojtabavahidinasab@grrlz.net>
2025-07-25 12:43:25 +0330

    بروزرسانی

--- config.h
+++ config.h
@@ -6,24 +6,24 @@ static const Block blocks[] = {
 	{"",	"sb-tasks",	10,	26},
 	/* {"",	"sb-music",	0,	11}, */
 	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-news",		0,	6},
+	/*{"",	"sb-news",		0,	6},*/
 	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
 	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
 	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
-	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
+	/*{"",	"sb-torrent",	20,	7},*/
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpu",		10,	18},
+	{"",	"sb-moonphase",	18000,	17},
+	/*{"",	"sb-doppler",	0,	13},*/
 	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
+	/*{"",	"sb-mailbox",	180,	12},*/
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	/* {"",	"sb-iplocate", 0,	27}, */
+	/*{"",	"sb-iplocate", 0,	27},*/
 	{"",	"sb-help-icon",	0,	15},
 };
 
@@ -34,3 +34,4 @@ static char *delim = " ";
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
